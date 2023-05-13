0e7d29907ad67eeb7396e3c1e56596199f7c2e37
bswlkh0310 <hhhello0507@dgsw.hs.kr>
2023-05-14 04:00:17 +0900

    5/14

---
+++ justMakeBoard/src/main/java/com/study/justMakeBoard/JustMakeBoardApplication.java
@@ -0,0 +1,13 @@
+package com.study.justMakeBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustMakeBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustMakeBoardApplication.class, args);
+	}
+
+}
---
+++ justMakeBoard/src/test/java/com/study/justMakeBoard/JustMakeBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.justMakeBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustMakeBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
