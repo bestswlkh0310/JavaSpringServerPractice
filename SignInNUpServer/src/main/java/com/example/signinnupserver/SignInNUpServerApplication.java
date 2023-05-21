7b97ec7122b496cd54bc94e0eed856d7c8f95a43
bswlkh0310 <hhhello0507@dgsw.hs.kr>
2023-05-21 21:48:59 +0900

    5/21

---
+++ SignInNUpServer/src/main/java/com/example/signinnupserver/SignInNUpServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.signinnupserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignInNUpServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SignInNUpServerApplication.class, args);
+    }
+
+}
---
+++ SignInNUpServer/src/test/java/com/example/signinnupserver/SignInNUpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.signinnupserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignInNUpServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
