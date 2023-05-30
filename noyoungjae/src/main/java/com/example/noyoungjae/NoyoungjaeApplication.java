3082201458510a111db009890bea139042cb732e
bswlkh0310 <hhhello0507@dgsw.hs.kr>
2023-05-30 23:02:39 +0900

    5/30

---
+++ noyoungjae/src/main/java/com/example/noyoungjae/NoyoungjaeApplication.java
@@ -0,0 +1,13 @@
+package com.example.noyoungjae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoyoungjaeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoyoungjaeApplication.class, args);
+    }
+
+}
---
+++ noyoungjae/src/test/java/com/example/noyoungjae/NoyoungjaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.noyoungjae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoyoungjaeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
