65627dae2638c5d6a0bc791d01d62ebe8f905ae4
bswlkh0310 <hhhello0507@dgsw.hs.kr>
2023-05-31 16:09:39 +0900

    5/31

---
+++ noyoungjae/src/main/java/com/example/noyoungjae/controller/NYJController.java
@@ -0,0 +1,14 @@
+package com.example.noyoungjae.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class NYJController {
+    @GetMapping("/go")
+    public String go() {
+        return "go";
+    }
+}
